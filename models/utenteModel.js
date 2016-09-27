ee52714745bb869ec7d9455dc18bfcad5a38de39
Kappa <kappa89@gmail.com>
2016-09-27 23:13:06 +0200

    definito utente, aggiunto express

--- models/utenteModel.js
+++ models/utenteModel.js
@@ -5,8 +5,15 @@ var utenteSchema = new Schema({
 	'admin' : { type: Boolean, default: false },
 	'nome' : { type: String, unique: true },
 	'punti' : { type: Number, default: 0 },
-	'accessi' : Array,
-	'transazioni' : Array
+	'accessi' : [{
+		'ingresso': Date,
+		'uscita': Date }],
+	'transazioni' : [{
+		'tipoTransazione': String, //TODO inserire default: vendo/acquisto
+		'oggetto': String,
+		'quantita': Number,
+		'Data': { type: Date, default: Date.now()
+		}}]
 });
 
 module.exports = mongoose.model('utente', utenteSchema);
