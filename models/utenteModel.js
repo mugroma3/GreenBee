70dea43e69a11431b61404623252f19eab765061
Kappa <kappa89@gmail.com>
2016-09-27 22:45:21 +0200

    modifiche valore default

--- models/utenteModel.js
+++ models/utenteModel.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var utenteSchema = new Schema({	'admin' : Boolean,	'nome' : String,	'punti' : Number,	'accessi' : Array,	'transazioni' : Array});
+var utenteSchema = new Schema({
+	'admin' : { type: Boolean, default: false },
+	'nome' : { type: String, unique: true },
+	'punti' : { type: Number, default: 0 },
+	'accessi' : Array,
+	'transazioni' : Array
+});
 
 module.exports = mongoose.model('utente', utenteSchema);
