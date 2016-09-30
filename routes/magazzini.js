1a982024996c64ab0f450490bc4df4751a314576
marco cappello <kappa89@gmail.com>
2016-09-30 11:45:02 +0200

    Rifatto magazzino e magazzinoController

--- routes/magazzini.js
+++ routes/magazzini.js
@@ -1,33 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var magazzinoController = require('../controllers/magazzinoController.js');
-
+var REST = require('../utils/REST');
 /*
  * GET
  */
 router.get('/', function (req, res) {
-    magazzinoController.list(req, res);
+    magazzinoController.list(options, function(answer){
+        REST.generate(req, res, answer);
+    });
 });
 
 /*
  * GET
  */
 router.get('/:id', function (req, res) {
-    magazzinoController.show(req, res);
-});
-
-/*
- * POST
- */
-router.post('/', function (req, res) {
-    magazzinoController.create(req, res);
-});
-
-/*
- * PUT
- */
-router.put('/:id', function (req, res) {
-    magazzinoController.update(req, res);
+    var options = {id: req.params.id};
+    magazzinoController.show(options, function(answer){
+        REST.generate(req, res, answer);
+    });
 });
 
 module.exports = router;
