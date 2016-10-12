baa0e1d54d5c98bd77de65a4d7f2c84b0290eae2
vitus89 <cherubini.andrea@outlook.com>
2016-10-12 22:14:02 +0200

    SENSORI E CENTRALINE GRAFICATI

--- public/javaScript/chart.js
+++ public/javaScript/chart.js
@@ -1,10 +1,16 @@
 /**
  * Created by vitus89 on 12/10/16.
  */
-function chartCreate(input){
-    var chart = new CanvasJS.Chart(input.nome, {
+function chartCentralina(input){
+    var battery_centralina = [];
+    var luminosity_centralina = [];
+    for (var i = 0; i < input.length; i++) {
+        battery_centralina.push({x: new Date(input[i].Data), y: input[i].battery_lvl});
+        luminosity_centralina.push({x: new Date(input[i].Data), y: input[i].luminosity});
+    }
+    var chart = new CanvasJS.Chart(input[0].name, {
         title: {
-            text: input.text,
+            text: "Dati della centralina " + input[0].name,
             fontSize: 30
         },
         animationEnabled: true,
@@ -25,19 +31,111 @@ function chartCreate(input){
             verticalAlign: "center",
             horizontalAlign: "right"
         },
-        data: []
-    });
-    for(var i=0;i<input.data.length;i++){
-        var a = {
+        data: [
+            {
                 type: "line",
                 showInLegend: true,
                 lineThickness: 2,
-                name: input.data[i].name,
+                name: "Battery Level",
                 markerType: "square",
-                color: input.data[i].color,
-                dataPoints: input.data[i].dataPoints
-            };
-        chart.options.data.push(a);
+                color: "#00ff00",
+                dataPoints: battery_centralina
+            },
+            {
+                type: "line",
+                showInLegend: true,
+                name: "Luminosity",
+                color: "#170fe7",
+                lineThickness: 2,
+                dataPoints: luminosity_centralina
+            }
+        ]
+    });
+    return chart;
+}
+
+function chartSensore(input, indexSensore){
+    var battery_ble = [];
+    var humidity_gnd_ble = [];
+    var humidity_air_ble = [];
+    var ph_ble = [];
+    var temperature_ble = [];
+    for (var i = 0; i < input.length; i++) {
+        if(input[i].ble_servers[indexSensore].reachable == 'yes'){
+            battery_ble.push({x: new Date(input[i].Data), y: input[i].ble_servers[indexSensore].battery_lvl});
+            humidity_gnd_ble.push({x: new Date(input[i].Data), y: input[i].ble_servers[indexSensore].humidity_gnd});
+            humidity_air_ble.push({x: new Date(input[i].Data), y: input[i].ble_servers[indexSensore].humidity_air});
+            ph_ble.push({x: new Date(input[i].Data), y: input[i].ble_servers[indexSensore].ph});
+            temperature_ble.push({x: new Date(input[i].Data), y: input[i].ble_servers[indexSensore].temperature});
+        }
     }
+    var chart = new CanvasJS.Chart(input[0].ble_servers[indexSensore].id_ble, {
+        title: {
+            text: "Dati del sensore " + input[0].ble_servers[indexSensore].id_ble,
+            fontSize: 30
+        },
+        animationEnabled: true,
+        axisX: {
+            gridColor: "Silver",
+            tickColor: "silver",
+            valueFormatString: "DD/MMM HH:MM"
+        },
+        legend: {
+            verticalAlign: "center",
+            horizontalAlign: "right"
+        },
+        toolTip: {
+            shared: true
+        },
+        theme: "theme2",
+        axisY: {
+            gridColor: "Silver",
+            tickColor: "silver"
+        },
+
+        data: [
+            {
+                type: "line",
+                showInLegend: true,
+                lineThickness: 2,
+                name: "Battery Level",
+                markerType: "square",
+                color: "#00ff00",
+                dataPoints: battery_ble
+            },
+            {
+                type: "line",
+                showInLegend: true,
+                name: "Humidity Land",
+                color: "#6b6e92",
+                lineThickness: 2,
+                dataPoints: humidity_gnd_ble
+            },
+            {
+                type: "line",
+                showInLegend: true,
+                name: "Humidity Air",
+                color: "#170fe7",
+                lineThickness: 2,
+                dataPoints: humidity_air_ble
+            },
+            {
+                type: "line",
+                showInLegend: true,
+                name: "PH",
+                color: "#31698a",
+                lineThickness: 2,
+                dataPoints: ph_ble
+            },
+            {
+                type: "line",
+                showInLegend: true,
+                name: "Temperature",
+                color: "#f4200d",
+                lineThickness: 2,
+                dataPoints: temperature_ble
+            }
+        ]
+    });
     return chart;
 }
