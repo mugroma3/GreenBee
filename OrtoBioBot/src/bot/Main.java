f9c65c4fa8d0dde7335937ea763eda5e0eb4f131
Jaeger87 <bad_hunter_666@yahoo.it>
2016-10-13 16:10:48 +0200

    token greenbee
    huhu

--- OrtoBioBot/src/bot/Main.java
+++ OrtoBioBot/src/bot/Main.java
@@ -5,7 +5,7 @@ import com.botticelli.messagereceiver.MessageReceiver;
 public class Main {
 	
 	private static String token = "211365415:AAFhGAjqhfYsc0hKoFB8UD9uwn1Dv24xweU";
-	
+	//private static String token = "262134854:AAFt2QT4BUSC-mFOMIw33zOd9Fr8J-SVKgE"; //greenBee
     public static void main(String[] args) {
 		OrtoBioBot ortoBot = new OrtoBioBot(token);
 		MessageReceiver mr = new MessageReceiver(ortoBot, 400, 3);
