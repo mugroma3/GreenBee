829479dd91bcae2fbd2ea0f71c7d18f49db12659
Marco Sbaffoni <maffo.anima87@gmail.com>
2016-10-13 13:57:12 +0200

    TruffaAggiornata

--- OrtoBioBot/src/apinod/TruffaBaruffa.java
+++ OrtoBioBot/src/apinod/TruffaBaruffa.java
@@ -12,11 +12,122 @@ import oggettijson.Transazione;
 
 public class TruffaBaruffa extends APINod{
 
+	List<ItemMag> resultMag;
+	List<Transazione> resultTrans;
+	Punti p;
+
 	public TruffaBaruffa(String tb) {
 		super(tb);
+		ItemMag t1=new ItemMag();
+		ItemMag t2=new ItemMag();
+		ItemMag t3=new ItemMag();
+		ItemMag t4=new ItemMag();
+		ItemMag t5=new ItemMag();
+		t1.setId("t1");
+		t1.setCosto(0);
+		t1.setImmagine("");
+		t1.setNome("Peperoni");
+		t1.setQuantita(4);
+		t1.setTelegramRefImg("");
+
+		t2.setId("t2");
+		t2.setCosto(0);
+		t2.setImmagine("");
+		t2.setNome("Pomodori");
+		t2.setQuantita(15);
+		t2.setTelegramRefImg("");
+
+		t3.setId("t3");
+		t3.setCosto(0);
+		t3.setImmagine("");
+		t3.setNome("Zucchine");
+		t3.setQuantita(4);
+		t3.setTelegramRefImg("");
+
+		t4.setId("t4");
+		t4.setCosto(0);
+		t4.setImmagine("");
+		t4.setNome("Carciofi");
+		t4.setQuantita(10);
+		t4.setTelegramRefImg("");
+
+		t5.setId("t5");
+		t5.setCosto(0);
+		t5.setImmagine("");
+		t5.setNome("Patate");
+		t5.setQuantita(12);
+		t5.setTelegramRefImg("");
+
+		this.resultMag.add(t1);
+		this.resultMag.add(t2);
+		this.resultMag.add(t3);
+		this.resultMag.add(t4);
+		this.resultMag.add(t5);
+
+
+		Transazione tr1,tr2,tr3,tr4,tr5;
+
+		tr1 = new Transazione();
+		tr2 = new Transazione();
+		tr3 = new Transazione();
+		tr4 = new Transazione();
+		tr5 = new Transazione();
+
+		tr1.setData("10 Ottobre 2016 10:25");
+		tr1.setOggetto("Pomodori");
+		tr1.setQuantita(4.0);
+		tr1.setTipoTransazione(TipoTransazione.vendo);
+
+		tr2.setData("10 Ottobre 2016 10:25");
+		tr2.setOggetto("Zucchine");
+		tr2.setQuantita(1.0);
+		tr2.setTipoTransazione(TipoTransazione.vendo);
+
+		tr3.setData("12 Ottobre 2016 17:45");
+		tr3.setOggetto("Peperoni");
+		tr3.setQuantita(1.0);
+		tr3.setTipoTransazione(TipoTransazione.acquisto);
+
+		tr4.setData("14 Ottobre 2016 10:25");
+		tr4.setOggetto("Cipolle");
+		tr4.setQuantita(2.0);
+		tr4.setTipoTransazione(TipoTransazione.acquisto);
+
+		tr5.setData("14 Ottobre 2016 13:25");
+		tr1.setOggetto("Patate");
+		tr1.setQuantita(24.0);
+		tr1.setTipoTransazione(TipoTransazione.vendo);
+
+		this.resultTrans = new ArrayList<Transazione>();
+
+		this.resultTrans.add(tr1);
+		this.resultTrans.add(tr2);		
+		this.resultTrans.add(tr3);
+		this.resultTrans.add(tr4);
+		this.resultTrans.add(tr5);
+
+		p = new Punti();
+		p.setPunti(10000);
+
 	}
 	
-	
+	public List<ItemMag> getResultMag() {
+		return resultMag;
+	}
+
+
+
+	public List<Transazione> getResultTrans() {
+		return resultTrans;
+	}
+
+
+
+	public Punti getP() {
+		return p;
+	}
+
+
 
 	@Override
 	public boolean isNellOrto(long id) {
@@ -34,122 +145,31 @@ public class TruffaBaruffa extends APINod{
 	}
 
 	@Override
-	public List<ItemMag> getMagazzino() {
-		List<ItemMag> result = new ArrayList<ItemMag>();
-		ItemMag t1=new ItemMag();
-		ItemMag t2=new ItemMag();
-		ItemMag t3=new ItemMag();
-		ItemMag t4=new ItemMag();
-		ItemMag t5=new ItemMag();
-		
-		t1.setId("t1");
-		t1.setCosto(0);
-		t1.setImmagine("");
-		t1.setNome("Peperoni");
-		t1.setQuantita(4);
-		t1.setTelegramRefImg("");
-		
-		t2.setId("t2");
-		t2.setCosto(0);
-		t2.setImmagine("");
-		t2.setNome("Pomodori");
-		t2.setQuantita(15);
-		t2.setTelegramRefImg("");
-		
-		t3.setId("t3");
-		t3.setCosto(0);
-		t3.setImmagine("");
-		t3.setNome("Zucchine");
-		t3.setQuantita(4);
-		t3.setTelegramRefImg("");
-		
-		t4.setId("t4");
-		t4.setCosto(0);
-		t4.setImmagine("");
-		t4.setNome("Carciofi");
-		t4.setQuantita(10);
-		t4.setTelegramRefImg("");
-		
-		t5.setId("t5");
-		t5.setCosto(0);
-		t5.setImmagine("");
-		t5.setNome("Patate");
-		t5.setQuantita(12);
-		t5.setTelegramRefImg("");
-		
-		result.add(t1);
-		result.add(t2);
-		result.add(t3);
-		result.add(t4);
-		result.add(t5);
-		
-		return result;
+	public List<ItemMag> getMagazzino() {	
+		return this.resultMag;
 	}
 
 	@Override
 	public List<Transazione> getStorico(long id) {
-		
-		List<Transazione> result = new ArrayList<Transazione>();
-		Transazione t1,t2,t3,t4,t5;
-		
-		t1 = new Transazione();
-		t2 = new Transazione();
-		t3 = new Transazione();
-		t4 = new Transazione();
-		t5 = new Transazione();
-		
-		t1.setData("10 Ottobre 2016 10:25");
-		t1.setOggetto("Pomodori");
-		t1.setQuantita(4.0);
-		t1.setTipoTransazione(TipoTransazione.vendo);
-		
-		t2.setData("10 Ottobre 2016 10:25");
-		t2.setOggetto("Zucchine");
-		t2.setQuantita(1.0);
-		t2.setTipoTransazione(TipoTransazione.vendo);
-		
-		t3.setData("12 Ottobre 2016 17:45");
-		t3.setOggetto("Peperoni");
-		t3.setQuantita(1.0);
-		t3.setTipoTransazione(TipoTransazione.acquisto);
-		
-		t4.setData("14 Ottobre 2016 10:25");
-		t4.setOggetto("Cipolle");
-		t4.setQuantita(2.0);
-		t4.setTipoTransazione(TipoTransazione.acquisto);
-		
-		t5.setData("14 Ottobre 2016 13:25");
-		t1.setOggetto("Patate");
-		t1.setQuantita(24.0);
-		t1.setTipoTransazione(TipoTransazione.vendo);
-		
-		result.add(t1);
-		result.add(t2);		
-		result.add(t3);
-		result.add(t4);
-		result.add(t5);
-		
-		return result;
+		return this.resultTrans;
 	}
-	
+
 	@Override
 	public Transazione addTransazione(long id, RichiestaTransazione rt)
 	{
-		Transazione result = new Transazione();
-		result.setData("30 Febbraio 2030");
-		result.setOggetto(rt.getOggetto());
-		result.setQuantita(rt.getQuantita()+0.0);
-		result.setTipoTransazione(rt.getTipoTransazione());
-		
-		return result;
+		Transazione resultNewTrans = new Transazione();
+		resultNewTrans.setData("30 Febbraio 2030");
+		resultNewTrans.setOggetto(rt.getOggetto());
+		resultNewTrans.setQuantita(rt.getQuantita()+0.0);
+		resultNewTrans.setTipoTransazione(rt.getTipoTransazione());
+
+		return resultNewTrans;
 	}
-	
+
 	@Override
 	public Punti getPunti(long id)
 	{
-		Punti p = new Punti();
-		p.setPunti(10000);
-		return p;
+		return this.p;
 	}
 
 }
