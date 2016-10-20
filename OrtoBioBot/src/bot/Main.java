1a1ee2eb43077bc0992e778303b20736e8c13d72
Marco Sbaffoni <maffo.anima87@gmail.com>
2016-10-20 10:40:48 +0200

    cleaning

--- OrtoBioBot/src/bot/Main.java
+++ OrtoBioBot/src/bot/Main.java
@@ -4,8 +4,8 @@ import com.botticelli.messagereceiver.MessageReceiver;
 
 public class Main {
 	
-	private static String token = "211365415:AAFhGAjqhfYsc0hKoFB8UD9uwn1Dv24xweU";
-	//private static String token = "262134854:AAFt2QT4BUSC-mFOMIw33zOd9Fr8J-SVKgE"; //greenBee
+	//private static String token = "test";
+	private static String token = "greenbee"; //greenBee
     public static void main(String[] args) {
 		OrtoBioBot ortoBot = new OrtoBioBot(token);
 		MessageReceiver mr = new MessageReceiver(ortoBot, 400, 3);
